74e9e544380e3ffc446b7370e4fa8321d8c35d83
Daniel <daniekie@studetn.ethz.ch>
2020-10-08 09:07:14 +0200

    -Debugged all bugs from last commit
    Problem
    -DAta needs not to contain Nan before outlier detection ??? Otherwise fiiting fails

--- 1_Project/main.py
+++ 1_Project/main.py
@@ -1,21 +1,34 @@
 import pandas as pd
-from sklearn.linear_model import Lasso, Ridge
-from sklearn.model_selection import GridSearchCV
-
+import xgboost as xgb
+import numpy as np
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Normalizer, StandardScaler, RobustScaler
 from sklearn.impute import SimpleImputer
-from sklearn.feature_selection import VarianceThreshold, SelectFromModel
-
-import xgboost as xgb
+from sklearn.neighbors import LocalOutlierFactor
 
 # Import data
-from sklearn.svm import SVR
-
 sample = pd.read_csv('raw/sample.csv')
-X_test = pd.read_csv('raw/X_test.csv')
-X_train = pd.read_csv('raw/X_train.csv')
-y_train = pd.read_csv('raw/y_train.csv')
+X_test = pd.read_csv('raw/X_test.csv', index_col='id')
+X_train = pd.read_csv('raw/X_train.csv', index_col='id')
+y_train = pd.read_csv('raw/y_train.csv', index_col='id')
+print('Data shape before outlier detection', X_train.shape, y_train.shape)
+
+
+# Outlier detection
+loc = LocalOutlierFactor()
+outliers = loc.fit_predict(X_train)
+# select all rows that are not outliers
+mask = outliers != -1
+X_train, y_train = X_train[mask, :], y_train[mask]
+# summarize the shape of the updated training dataset
+print('Training data shape', X_train.shape, y_train.shape)
+
+
+# Reduce Data for debugging
+if 1:
+    [X_train, a, y_train, b] = train_test_split(X_train, y_train, test_size=0.99)
+    del a, b
 
 # Inspect data
 percent_missing = X_test.isnull().sum() * 100 / len(X_test)
@@ -24,33 +37,33 @@ missing_value_df = pd.DataFrame({'column_name': X_test.columns,
 missing_value_df.sort_values('percent_missing', inplace=True)
 # X_train is missing 3%-10% of the values
 
+
 # Create pipeline
 pipe = Pipeline([
     # the scale stage is populated by the param_grid
     ('scale', 'passthrough'),
     ('impute', SimpleImputer()),
-    ('feature_selection', SelectFromModel(Lasso())),
-    ('estimation', 'passthrough')
+    ('estimation', xgb.XGBRegressor())
 ])
+
 
 # Specify parameters to be searched over
 param_grid = [
-    {
+     {
         'scale': [Normalizer(), StandardScaler(), RobustScaler()],
         'impute__strategy': ['mean', 'median', 'most_frequent'],
-        'feature_selection__estimator__alpha': [0.1, 1, 10],
-        'estimation': [Ridge(alpha=0.1),Ridge(alpha=1), Ridge(alpha=10), SVR()]
-    }, {
-        'scale': [Normalizer(), StandardScaler(), RobustScaler()],
-        'impute__strategy': ['mean', 'median', 'most_frequent'],
-        'estimation': [xgb.XGBRegressor()]
-    }
+        'estimation__max_depth':[6],  # [2, 6, 8],
+        'estimation__gamma': [0],  # [0, 2, 8],
+        'estimation__min_child_weight': [1]  # [1, 4, 8]
+        }
 ]
+
 
 # Gridsearch
 search = GridSearchCV(pipe, param_grid=param_grid, n_jobs=-1, scoring='r2')
 print("Starting Gridsearch")
 search.fit(X_train, y_train)
+
 
 # Evaluate Results
 print("Best parameters set found on development set:")
@@ -67,10 +80,13 @@ for mean, std, params in zip(means, stds, search.cv_results_['params']):
           % (mean, std * 2, params))
 print()
 
+
 # Predict for test set
 y_test = search.predict(X_test)
 print()
-y_test.to_csv('prediction.zip')
-print('Results saved as prediction.zip')
-# Save prediction
 
+
+# Save prediction
+y_test = pd.DataFrame(y_test)
+y_test.to_csv('prediction.csv', index_label='id', header=['y'], compression=None)
+print('Results saved as prediction.csv')
---
+++ 1_Project/outlier_extractor.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.pipeline import TransformerMixin
+
+
+class OutlierExtractor(TransformerMixin):
+    def __init__(self, **kwargs):
+        """
+        Create a transformer to remove outliers. A threshold is set for selection
+        criteria, and further arguments are passed to the LocalOutlierFactor class
+
+        Keyword Args:
+            neg_conf_val (float): The threshold for excluding samples with a lower
+               negative outlier factor.
+
+        Returns:
+            object: to be used as a transformer method as part of Pipeline()
+        """
+
+        self.threshold = kwargs.pop('neg_conf_val', -10.0)
+
+        self.kwargs = kwargs
+
+    def transform(self, X, y):
+        """
+        Uses LocalOutlierFactor class to subselect data based on some threshold
+
+        Returns:
+            ndarray: subsampled data
+
+        Notes:
+            X should be of shape (n_samples, n_features)
+        """
+        X = np.asarray(X)
+        y = np.asarray(y)
+        lcf = LocalOutlierFactor(**self.kwargs)
+        lcf.fit(X)
+        return (X[lcf.negative_outlier_factor_ > self.threshold, :],
+                y[lcf.negative_outlier_factor_ > self.threshold])
+
+    def fit(self, *args, **kwargs):
+        return self
