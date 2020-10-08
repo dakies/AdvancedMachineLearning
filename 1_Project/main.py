7323b49bbd93f6dbe4fb7ea512591632fda83b75
Daniel <daniekie@studetn.ethz.ch>
2020-10-08 10:03:28 +0200

    Added imblearn pipe adn outlier rejection

--- 1_Project/main.py
+++ 1_Project/main.py
@@ -2,10 +2,24 @@ import pandas as pd
 import xgboost as xgb
 import numpy as np
 from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import Normalizer, StandardScaler, RobustScaler
 from sklearn.impute import SimpleImputer
 from sklearn.neighbors import LocalOutlierFactor
+from sklearn.ensemble import IsolationForest
+
+from imblearn.pipeline import Pipeline
+from imblearn import FunctionSampler
+
+
+def outlier_rejection(X, y):
+    """This will be our function used to resample our dataset."""
+    print('Initiating Outlier detection')
+    model = IsolationForest()
+    y_pred = model.fit_predict(X)
+    print('Outlier detection successful')
+    print('Training data shape', X.shape, y.shape)
+    return X[y_pred == 1], y[y_pred == 1]
+
 
 # Import data
 sample = pd.read_csv('raw/sample.csv')
@@ -14,19 +28,8 @@ X_train = pd.read_csv('raw/X_train.csv', index_col='id')
 y_train = pd.read_csv('raw/y_train.csv', index_col='id')
 print('Data shape before outlier detection', X_train.shape, y_train.shape)
 
-
-# Outlier detection
-loc = LocalOutlierFactor()
-outliers = loc.fit_predict(X_train)
-# select all rows that are not outliers
-mask = outliers != -1
-X_train, y_train = X_train[mask, :], y_train[mask]
-# summarize the shape of the updated training dataset
-print('Training data shape', X_train.shape, y_train.shape)
-
-
 # Reduce Data for debugging
-if 1:
+if 0:
     [X_train, a, y_train, b] = train_test_split(X_train, y_train, test_size=0.99)
     del a, b
 
@@ -37,33 +40,30 @@ missing_value_df = pd.DataFrame({'column_name': X_test.columns,
 missing_value_df.sort_values('percent_missing', inplace=True)
 # X_train is missing 3%-10% of the values
 
-
 # Create pipeline
 pipe = Pipeline([
     # the scale stage is populated by the param_grid
     ('scale', 'passthrough'),
     ('impute', SimpleImputer()),
+    ('outlier', FunctionSampler(func=outlier_rejection)),
     ('estimation', xgb.XGBRegressor())
 ])
 
-
 # Specify parameters to be searched over
 param_grid = [
-     {
+    {
         'scale': [Normalizer(), StandardScaler(), RobustScaler()],
         'impute__strategy': ['mean', 'median', 'most_frequent'],
-        'estimation__max_depth':[6],  # [2, 6, 8],
+        'estimation__max_depth': [6],  # [2, 6, 8],
         'estimation__gamma': [0],  # [0, 2, 8],
         'estimation__min_child_weight': [1]  # [1, 4, 8]
-        }
+    }
 ]
-
 
 # Gridsearch
 search = GridSearchCV(pipe, param_grid=param_grid, n_jobs=-1, scoring='r2')
 print("Starting Gridsearch")
 search.fit(X_train, y_train)
-
 
 # Evaluate Results
 print("Best parameters set found on development set:")
@@ -80,11 +80,9 @@ for mean, std, params in zip(means, stds, search.cv_results_['params']):
           % (mean, std * 2, params))
 print()
 
-
 # Predict for test set
 y_test = search.predict(X_test)
 print()
-
 
 # Save prediction
 y_test = pd.DataFrame(y_test)
