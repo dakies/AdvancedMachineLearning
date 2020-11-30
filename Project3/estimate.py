0fd6a03454555205672528be475d09ae9a235d2f
Daniel <daniekie@studetn.ethz.ch>
2020-11-30 12:13:13 +0100

    New features

--- Project3/estimate.py
+++ Project3/estimate.py
@@ -2,7 +2,6 @@ from pprint import pprint
 from time import time
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 from imblearn import FunctionSampler
 from imblearn.pipeline import Pipeline
@@ -15,8 +14,7 @@ from sklearn.preprocessing import RobustScaler
 from sklearn.svm import SVC
 from skopt import BayesSearchCV
 from skopt.space import Categorical, Integer, Real
-from xgboost import XGBClassifier
-from xgboost import plot_importance
+from xgboost import XGBClassifier, plot_importance
 
 
 def lof(x, y):
@@ -32,30 +30,35 @@ def isof(x, y, cont='auto'):
     return x[y_pred == 1], y[y_pred == 1]
 
 
+def rmcorr(x_train, x_test):
+    # Remove correlated features
+    # Has a bug
+    correlated_features = set()
+    correlation_matrix = x_train.corr()
+
+    for i in range(len(correlation_matrix.columns)):
+        for j in range(i):
+            if abs(correlation_matrix.iloc[i, j]) > 0.9:
+                colname = correlation_matrix.columns[i]
+                correlated_features.add(colname)
+
+    print('Correlated features')
+    print(len(correlated_features))
+
+    X_train = x_train.drop(labels=correlated_features, axis=1, inplace=True)
+    X_test = x_test.drop(labels=correlated_features, axis=1, inplace=True)
+    return X_train, X_test
+
+
 # Load data
 X_test = pd.read_csv('final/test_127.csv')
 X_train = pd.read_csv('final/train_127.csv')
 y_train = pd.read_csv('raw/y_train.csv', index_col='id')
 
-# Remove correlated features
-# correlated_features = set()
-# correlation_matrix = X_train.corr()
-#
-# for i in range(len(correlation_matrix.columns)):
-#     for j in range(i):
-#         if abs(correlation_matrix.iloc[i, j]) > 0.8:
-#             colname = correlation_matrix.columns[i]
-#             correlated_features.add(colname)
-
-# print('Correlated features')
-# print(len(correlated_features))
-#
-# X_train = X_train.drop(labels=correlated_features, axis=1, inplace=True)
-# X_test = X_test.drop(labels=correlated_features, axis=1, inplace=True)
+caner_test = pd.read_csv('feat_caner/features_test_43.csv')
+caner_train = pd.read_csv('feat_caner/features_train_43.csv')
 
 # Todo how many infs and nans are there?
-X_train = X_train.replace([np.inf], 100000)
-X_test = X_test.replace([-np.inf], -100000)
 # si = SimpleImputer(strategy='median')
 # X_train = si.fit_transform(X_train)
 # X_test = si.transform(X_test)
@@ -66,6 +69,7 @@ percent_missing = X_train.isnull().sum() * 100 / len(X_train)
 missing_value_df = pd.DataFrame({'column_name': X_train.columns,
                                  'percent_missing': percent_missing})
 missing_value_df.sort_values('percent_missing', inplace=True)
+
 # print(missing_value_df)
 
 # for label, content in X_train.iteritems():
@@ -82,17 +86,22 @@ X_test = X_test.drop(
     ['HRV_LFn', 'HRV_SampEn', 'HRV_LF', 'HRV_LFHF', 'HRV_VLF', 'HRV_ULF', 'heart_rate', 'HRV_HF', 'HRV_VHF', 'HRV_HFn',
      'HRV_LnHF'], axis=1)
 
+X_train = pd.concat([X_train, caner_train.iloc[:, 28:]], axis=1)
+X_test = pd.concat([X_test, caner_test.iloc[:, 28:]], axis=1)
+# X_train, X_test = rmcorr(X_train, X_test)
+
 print(X_train.isnull().values.any())
 print(y_train.isnull().values.any())
 print()
 print(X_train.shape)
 print(y_train.shape)
+
 pseudotest = False
 if pseudotest:
-    X_train, X_test_val, y_train, y_test_val = train_test_split(X_train, y_train, test_size=0.33, random_state=42)
+    X_train, X_test_val, y_train, y_test_val = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
 
 # Reduce Data for debugging
-evals = 200
+evals = 10
 if 0:
     [X_train, a, y_train, b] = train_test_split(X_train, y_train, test_size=0.9)
     del a, b
@@ -120,11 +129,12 @@ svc_search = {
 
 xgb_search = {
     'model': [XGBClassifier()],
-    'model__learning_rate': (0.01, 1.0, 'log-uniform'),
+    'scale': ['passthrough'],
     # 'model__min_child_weight': (0, 10),
     # 'model__max_delta_step': Integer(0, 20),
     # 'model__colsample_bytree': (0.01, 1.0, 'uniform'),
     # 'model__colsample_bylevel': (0.01, 1.0, 'uniform'),
+    'model__learning_rate': (0.01, 1.0, 'log-uniform'),
     'model__n_estimators': Integer(50, 150),
     'model__scale_pos_weight': Real(1, 1000, 'log-uniform'),
     'model__min_child_weight': Integer(1, 10),
@@ -132,6 +142,25 @@ xgb_search = {
     'model__subsample': Real(0.3, 1),
     'model__colsample_bytree': Real(0.3, 1),
     'model__max_depth': Integer(3, 20)
+}
+
+xg = XGBClassifier()
+xg.load_model('model_file_name.json')
+xgb_search_prev = {
+    'model': [xg],
+    # 'model__learning_rate': (0.01, 1.0, 'log-uniform'),
+    # 'model__min_child_weight': (0, 10),
+    # 'model__max_delta_step': Integer(0, 20),
+    # 'model__colsample_bytree': (0.01, 1.0, 'uniform'),
+    # 'model__colsample_bylevel': (0.01, 1.0, 'uniform'),
+
+    # 'model__n_estimators': Integer(100, 200),
+    # 'model__scale_pos_weight': Real(1, 1000, 'log-uniform'),
+    # 'model__min_child_weight': Integer(1, 10),
+    # 'model__gamma': Integer(1, 5),
+    # 'model__subsample': Real(0.3, 1),
+    # 'model__colsample_bytree': Real(0.1, 1),
+    # 'model__max_depth': Integer(6, 12)
 }
 
 xgb_pca_search = {
@@ -145,7 +174,7 @@ xgb_pca_search = {
 opt = BayesSearchCV(
     pipe,
     # (parameter space, # of evaluations)
-    [(xgb_search, evals)],
+    [(xgb_search_prev, evals)],
     cv=3,
     scoring='f1_micro',
     n_jobs=-1,
